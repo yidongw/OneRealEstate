da43a92f7c8fa88fb8ff4c4e8e849766579a7fa2
Alan Wang <wy.dong96@gmail.com>
2018-08-05 19:49:46 -0700

    api

--- contract/houserecords2.sol
+++ contract/houserecords2.sol
@@ -1,28 +1,28 @@
 pragma solidity ^0.4.18;
 
 contract HouseRecords2 {
-    
+
     struct Record {
         uint id;
         string date;
         string description;
         string documentHash;
     }
-    
+
     mapping (uint => Record) Records;
     uint[] public RecordsKeys;
-    
-    function setRecord(uint _listingid, uint _id, string _date, string _description, string _documentHash) public {
-        Record storage myRecord = Records[_listingid];
-        
-        myRecord.id = _id;
+
+    function setRecord(string _date, string _description, string _documentHash) public {
+        Record storage myRecord = Records[RecordsKeys.length];
+
+        myRecord.id = RecordsKeys.length;
         myRecord.date = _date;
         myRecord.description = _description;
-        myRecord.documentHash = _documentHash;        
+        myRecord.documentHash = _documentHash;
 
-        RecordsKeys.push(_listingid) -1;
+        RecordsKeys.push(RecordsKeys.length);
     }
-    
+
     function getRecordByListingid(uint _listingid) view public returns (uint, string, string, string) {
         return (Records[_listingid].id, Records[_listingid].date, Records[_listingid].description, Records[_listingid].documentHash);
     }
@@ -30,9 +30,8 @@ contract HouseRecords2 {
     function getAllRecords() view public returns(uint[]) {
         return RecordsKeys;
     }
-        
+
     function countRecords() view public returns (uint) {
         return RecordsKeys.length;
     }
-    
 }
--- public/examples/quark_call.js
+++ public/examples/quark_call.js
@@ -14,160 +14,164 @@
   /** namespace */
   let quarkcall = {};
   quarkcall.abi = [
-    {
-      "constant": true,
-      "inputs": [
-        {
-          "name": "",
-          "type": "uint256"
-        }
-      ],
-      "name": "RecordsKeys",
-      "outputs": [
-        {
-          "name": "",
-          "type": "uint256"
-        }
-      ],
-      "payable": false,
-      "stateMutability": "view",
-      "type": "function"
-    },
-    {
-      "constant": true,
-      "inputs": [
-        {
-          "name": "_listingid",
-          "type": "uint256"
-        }
-      ],
-      "name": "getRecordByListingid",
-      "outputs": [
-        {
-          "name": "",
-          "type": "uint256"
-        },
-        {
-          "name": "",
-          "type": "string"
-        },
-        {
-          "name": "",
-          "type": "string"
-        },
-        {
-          "name": "",
-          "type": "string"
-        }
-      ],
-      "payable": false,
-      "stateMutability": "view",
-      "type": "function"
-    },
-    {
-      "constant": false,
-      "inputs": [
-        {
-          "name": "_listingid",
-          "type": "uint256"
-        },
-        {
-          "name": "_id",
-          "type": "uint256"
-        },
-        {
-          "name": "_date",
-          "type": "string"
-        },
-        {
-          "name": "_description",
-          "type": "string"
-        },
-        {
-          "name": "_documentHash",
-          "type": "string"
-        }
-      ],
-      "name": "setRecord",
-      "outputs": [],
-      "payable": false,
-      "stateMutability": "nonpayable",
-      "type": "function"
-    },
-    {
-      "constant": true,
-      "inputs": [],
-      "name": "getAllRecords",
-      "outputs": [
-        {
-          "name": "",
-          "type": "uint256[]"
-        }
-      ],
-      "payable": false,
-      "stateMutability": "view",
-      "type": "function"
-    },
-    {
-      "constant": true,
-      "inputs": [],
-      "name": "countRecords",
-      "outputs": [
-        {
-          "name": "",
-          "type": "uint256"
-        }
-      ],
-      "payable": false,
-      "stateMutability": "view",
-      "type": "function"
-    }
-  ];
+	{
+		"constant": false,
+		"inputs": [
+			{
+				"name": "_date",
+				"type": "string"
+			},
+			{
+				"name": "_description",
+				"type": "string"
+			},
+			{
+				"name": "_documentHash",
+				"type": "string"
+			}
+		],
+		"name": "setRecord",
+		"outputs": [],
+		"payable": false,
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"constant": true,
+		"inputs": [],
+		"name": "countRecords",
+		"outputs": [
+			{
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"payable": false,
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"constant": true,
+		"inputs": [],
+		"name": "getAllRecords",
+		"outputs": [
+			{
+				"name": "",
+				"type": "uint256[]"
+			}
+		],
+		"payable": false,
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"constant": true,
+		"inputs": [
+			{
+				"name": "_listingid",
+				"type": "uint256"
+			}
+		],
+		"name": "getRecordByListingid",
+		"outputs": [
+			{
+				"name": "",
+				"type": "uint256"
+			},
+			{
+				"name": "",
+				"type": "string"
+			},
+			{
+				"name": "",
+				"type": "string"
+			},
+			{
+				"name": "",
+				"type": "string"
+			}
+		],
+		"payable": false,
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"constant": true,
+		"inputs": [
+			{
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"name": "RecordsKeys",
+		"outputs": [
+			{
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"payable": false,
+		"stateMutability": "view",
+		"type": "function"
+	}
+];
+  // 29: 0x3B13FF8A04b5aA1BB1aD49AD792394309cc197633bb5491d
+  // 42: 0x3B13FF8A04b5aA1BB1aD49AD792394309cc197633bb5492A
 
-  quarkcall.listingid = 6;
-  quarkcall.contractAddress = '0xfD1d2734fB822524533BDA2C2Ff5C89A69c6E9070Aafc85D';
+  quarkcall.contractAddress = '0x9A74b48dd5e8e4a4B9ee3A5c7e6978299883143a3bb5491d';
 
-  async function sendTx() {
+  const ethAddr = web3.eth.accounts[0];
+  const qkcAddr = QuarkChain.getQkcAddressFromEthAddress(ethAddr);
 
-    const ethAddr = web3.eth.accounts[0];
-    const qkcAddr = QuarkChain.getQkcAddressFromEthAddress(ethAddr);
+  let sampleContract = web3.qkc.contract(quarkcall.abi);
 
-    let sampleContract = web3.qkc.contract(quarkcall.abi);
+  let contractAddress = quarkcall.contractAddress;
+  let sampleContractInstance = sampleContract.at(contractAddress);
 
-    let contractAddress = quarkcall.contractAddress;
-    let sampleContractInstance = sampleContract.at(contractAddress);
+  let shardId = '0x0Aafc85D'; // shard 29.  must include correct shard.
+  let readOption = {
+    fromFullShardId: shardId,
+  };
 
-    let shardId = '0x0Aafc85D'; // shard 29.  must include correct shard.
-    let readOption = {
-      fromFullShardId: shardId,
-    };
+  let writeOption = {
+    gas: 1000000,
+    fromFullShardId: shardId,
+    //from: web3.eth.accounts[0],
+    //value: web3.toWei(0.1, 'ether')
+  };
 
-    let writeOption = {
-      gas: 1000000,
-      fromFullShardId: shardId,
-      //from: web3.eth.accounts[0],
-      //value: web3.toWei(0.1, 'ether')
-    };
+  var total = 0;
 
-    sampleContractInstance.countRecords(readOption, (err, result) => {
+  async function countRecords() {
+    await sampleContractInstance.countRecords(readOption, (err, result) => {
       if (result != null) {
         console.log("countRecords: ", result.toNumber());
+        let x = result.toNumber()
+        console.log(x);
+        total = x;
       } else {
         console.err("countRecords errors");
       }
     });
+  }
 
-    sampleContractInstance.getRecordByListingid(6, readOption, (err, result) => {
+  async function getRecordByListingid(listingid) {
+    await sampleContractInstance.getRecordByListingid(listingid, readOption, (err, result) => {
       if (result != null) {
-        console.log("getAllRecords: ", result[0].toNumber(), result[1], result[2], result[3]);
+        console.log("getAllRecords...: ", result[0].toNumber(), result[1], result[2], result[3]);
+        console.log(result);
+        return result;
       } else {
         console.err("getAllRecords errors");
       }
     });
+  }
 
-    sampleContractInstance.getAllRecords(readOption, (err, result) => {
+  async function getAllRecords(){
+    await sampleContractInstance.getAllRecords(readOption, (err, result) => {
       if (result != null) {
-        let newResult = result.map((x) => { 
+        console.log(result);
+        let newResult = result.map((x) => {
           // if (typeof x === "BigNumber") {
           //   return x.toNumber();
           // } else {
@@ -180,72 +184,56 @@
         console.err("getAllRecords errors");
       }
     });
-
-    quarkcall.listingid++;
-    sampleContractInstance.setRecord(quarkcall.listingid, quarkcall.listingid, "07052018", "description2", "ipfs2", writeOption, console.log);
-
-    // Should be able to find tx ID in console.
-    //await web3.qkc.sendTransaction(rawTx, console.log);
   }
 
-  async function countRecords() {
+  async function setRecord() {
+    let d = new Date();
+    let tmp = "8/5/2018";
+    console.log(tmp);
 
-    const ethAddr = web3.eth.accounts[0];
-    const qkcAddr = QuarkChain.getQkcAddressFromEthAddress(ethAddr);
+    let des = document.getElementById("des").value;
+    let company = document.getElementById("company").value;
 
-    let sampleContract = web3.qkc.contract(quarkcall.abi);
+    des = des + " " + company;
 
-    let contractAddress = quarkcall.contractAddress;
-    let sampleContractInstance = sampleContract.at(contractAddress);
+    console.log(des);
 
-    let shardId = '0x0Aafc85D'; // shard 29.  must include correct shard.
-    let readOption = {
-      fromFullShardId: shardId,
-    };
+    await sampleContractInstance.setRecord(tmp, des, "Hash", writeOption, console.log);
+  }
 
-    let writeOption = {
-      gas: 1000000,
-      fromFullShardId: shardId,
-      //from: web3.eth.accounts[0],
-      //value: web3.toWei(0.1, 'ether')
-    };
 
-    sampleContractInstance.countRecords(readOption, (err, result) => {
+  async function render() {
+    await sampleContractInstance.countRecords(readOption, (err, result) => {
       if (result != null) {
-        console.log("countRecords: ", result.toNumber());
+        let total = result.toNumber();
+        console.log(total);
+        for (let i = 0; i < total; i++) {
+          let home = document.getElementById("home");
+          let p = document.createElement("p");
+          sampleContractInstance.getRecordByListingid(i, readOption, (err, result) => {
+            if (result != null) {
+              p.innerHTML = result[1] + " " +  result[2];
+            } else {
+              console.err("getAllRecords errors");
+            }
+          });
+          // p.innerHTML = res[i];
+          console.log(p);
+          home.appendChild(p);
+        }
+
+        console.log("countRecords: ", total);
       } else {
-        console.err("countRecords errors");
+        console.err("Render error");
       }
     });
   }
 
-  async function getRecordByListingid(listingid) {
+  render();
 
-    const ethAddr = web3.eth.accounts[0];
-    const qkcAddr = QuarkChain.getQkcAddressFromEthAddress(ethAddr);
-
-    let sampleContract = web3.qkc.contract(quarkcall.abi);
-
-    let contractAddress = quarkcall.contractAddress;
-    let sampleContractInstance = sampleContract.at(contractAddress);
-
-    let shardId = '0x0Aafc85D'; // shard 29.  must include correct shard.
-    let readOption = {
-      fromFullShardId: shardId,
-    };
-
-    let writeOption = {
-      gas: 1000000,
-      fromFullShardId: shardId,
-      //from: web3.eth.accounts[0],
-      //value: web3.toWei(0.1, 'ether')
-    };
-
-    sampleContractInstance.getRecordByListingid(6, readOption, (err, result) => {
-      if (result != null) {
-        console.log("getAllRecords: ", result[0].toNumber(), result[1], result[2], result[3]);
-      } else {
-        console.err("getAllRecords errors");
-      }
-    });
-  }
+  // console.log("here!!");
+  // console.log(countRecords());
+  // console.log("start get record id");
+  // console.log(getRecordByListingid(1));
+  // console.log("end get record id");
+  // console.log(getAllRecords());
